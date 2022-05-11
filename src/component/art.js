f26100ced7c35016e91f69830b2078b520de1e2f
Moatsem <m3tsmsh@gmail.com>
2022-05-12 00:04:30 +0200

    inishing art gallarey in website view

--- src/component/art.js
+++ src/component/art.js
@@ -4,52 +4,48 @@ import beeGirl from "../assets/myArt/beeGirl.png";
 import bunny from "../assets/myArt/bunny.png";
 import Artboard from "../assets/myArt/Artboard.png";
 import freeURbody from "../assets/myArt/freeURbody.png";
-import behance from "../assets/behance.png"
-import insta from "../assets/insta.png"
-
+import behance from "../assets/behance.png";
+import insta from "../assets/insta.png";
 
 function Art() {
   return (
-    <section className=" section">
-      <h1 className="colored-font big-font">My Art ,,</h1>
-<br></br>
+    <section className="  section">
+              <h1 className=" art-header  colored-font big-font">My Art ,,</h1>
+
       <div id="my-art">
 
         <img className="art-image" id="bee" src={beeGirl} alt="Bee character" />
-        <img className="art-image" id="free" src={freeURbody} alt="free your body" />
+        <img
+          className="art-image"
+          id="free"
+          src={freeURbody}
+          alt="free your body"
+        />
         <img className="art-image" id="art" src={Artboard} alt="art board" />
-        <img className="art-image" id="bunny" src={bunny} alt="bunny on motorcycle" />
+        <img
+          className="art-image"
+          id="bunny"
+          src={bunny}
+          alt="bunny on motorcycle"
+        />
 
         <img className="art-image" id="aqua" src={aqua} alt="astronuate aqua" />
 
         <img className="art-image" id="coffee" src={coffee} alt="cofee girl" />
-
-
       </div>
-      <br/>
-      <div className="center "> 
-      <span   className="colored-font  "> For More </span>
-      <div className="icon">
-
-      <a
-              href="https://www.behance.net/m3tsm"
-              target="_blank"
-             >
-              <embed src={behance} />
-            </a>
-            <a
-              href="https://www.instagram.com/tasim_sh/"
-              target="_blank"
-             >
-              <embed src={insta} />
-            </a>
+      <br />
+      <div className="center ">
+        <span className="colored-font  "> Find More </span>
+        <div className="icon">
+          <a href="https://www.behance.net/m3tsm" target="_blank">
+            <embed src={behance} />
+          </a>
+          <a href="https://www.instagram.com/tasim_sh/" target="_blank">
+            <embed src={insta} />
+          </a>
+        </div>
+       
       </div>
-     
-     
-      </div>
-
-
-      <br/><br/>
     </section>
   );
 }
